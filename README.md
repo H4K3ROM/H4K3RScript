3ccf92f740ec538d4b5558359b992f2d7a6b01d9
H4K3ROM <44916440+H4K3ROM@users.noreply.github.com>
2019-01-01 09:38:32 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # H4K3RScript
+Extra Build Scripts
+
+
+//////// Attempting to pull the sources from the H4K3ROM Repos ////////
+
+If you aren't starting with a fresh repo sync --force-sync this notice probably doesn't apply!
+
+Device OnePlus3T
---
+++ h4k3rom.sh
@@ -0,0 +1,9 @@
+cd ~/H4K3ROM/vendor/gzosp/H4K3RScript
+head -25 README.md
+cd ~/H4K3ROM/device
+rm -rf ~/H4K3ROM/device/oneplus
+rm -rf ~/H4K3ROM/device/oneplus/oneplus3
+mkdir ~/H4K3ROM/device/oneplus
+cd ~/H4K3ROM/device/oneplus
+git clone https://github.com/H4K3ROM/oneplus3.git
+cd ~/H4K3ROM
